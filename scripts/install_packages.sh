e46174a80ad89cf70f017a87a60b289d1493ce75
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-10 19:54:23 -0400

    Changes in shell file

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -4,6 +4,9 @@ echo "Installing NodeJS"
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+echo "Installing unzip"
+sudo apt install unzip
+
 echo "Installing mysql"
 sudo apt install mysql-client-core-8.0 -y 
 sudo apt install mysql-server -y
