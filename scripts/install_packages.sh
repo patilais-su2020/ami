2e8df045cc08dcb0fe90e15ae7e4acc8c1718ad4
Aishwarya Patil <patil.ais@northeastern.edu>
2020-06-28 16:05:22 -0400

    added code deploy agent installation

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -6,3 +6,24 @@ sudo apt-get install -y nodejs
 
 echo "Installing unzip"
 sudo apt install unzip
+
+echo "--------------- CodeDeploy Agent ---------------"
+sudo apt-get install ruby -y
+wget "https://aws-codedeploy-${AWS_REGION}.s3.${AWS_REGION}.amazonaws.com/latest/install"
+chmod +x ./install
+sudo ./install auto
+echo "###INFO: Checking code deplot agent status ##"
+sudo service codedeploy-agent status
+if [[ $? -ne 0 ]]; then
+    echo "WARNING: CodeDeploy agent is not up... Bringing it up..."
+    sudo service codedeploy-agent start
+    sudo service codedeploy-agent status
+fi
+echo "###INFO: Checking code deplot agent status again##"
+sudo service codedeploy-agent status
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: CodeDeploy agent is still not up. Exiting"
+    exit 1
+else
+    echo "SUCCESS: CodeDeploy agent is live"
+fi
