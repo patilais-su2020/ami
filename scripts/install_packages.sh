87f2a1c3ec1a65db729c57454fc9a5bcf5ba9f04
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-06 23:48:21 -0400

    Changes in cloud-watch-agent service

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -24,7 +24,7 @@ if [[ $? -ne 0 ]]; then
     sudo service codedeploy-agent start
     sudo service codedeploy-agent status
 fi
-echo "###INFO: Checking code deplot agent status again##"
+echo "###INFO: Checking code deploy agent status again##"
 sudo service codedeploy-agent status
 if [[ $? -ne 0 ]]; then
     echo "ERROR: CodeDeploy agent is still not up. Exiting"
@@ -34,7 +34,7 @@ else
 fi
 
 echo "------------------- Cloud Watch ------------------------"
-wget "https://s3.${AWS_REGION}.amazonaws.com/amazoncloudwatch-agent-${AWS_REGION}/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb"
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo usermod -aG adm cwagent
 
