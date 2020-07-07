459a194510b2e7c7835c50b30b6eccec54e0e4ed
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-07 00:21:45 -0400

    cloudwatch installation

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -33,10 +33,9 @@ else
     echo "SUCCESS: CodeDeploy agent is live"
 fi
 
-# echo "------------------- Cloud Watch ------------------------"
-# wget "https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb"
-# sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
-# sudo usermod -aG adm cwagent
+echo "------------------- Cloud Watch ------------------------"
+wget "https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb"
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 
-# sudo systemctl enable amazon-cloudwatch-agent.service    
-# sudo systemctl status amazon-cloudwatch-agent.service
+sudo systemctl enable amazon-cloudwatch-agent.service    
+sudo systemctl status amazon-cloudwatch-agent.service
