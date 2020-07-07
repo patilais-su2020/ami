d1de3b6e7ad2ec4a242d0229524e9fff7d00e446
Aishwarya Patil <patil.ais@northeastern.edu>
2020-07-07 00:07:31 -0400

    Changes in packer version

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -34,7 +34,7 @@ else
 fi
 
 echo "------------------- Cloud Watch ------------------------"
-wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+wget "https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb"
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 sudo usermod -aG adm cwagent
 
